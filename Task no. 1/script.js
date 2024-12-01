e1619220eb2c3e053a36e256625c49bcc5d60b4a
Shishir19999 <shishirsharma199@gmail.com>
2024-12-01 23:41:32 +0545

    task 2 complete

--- Task no. 1/script.js
+++ Task no. 1/script.js
@@ -9,5 +9,28 @@ $(document).ready(function () {
       slidesToShow: 1,
       slidesToScroll: 1,
     });
+    function updateImagesForMobile() {
+      const isMobile = window.matchMedia("(max-width: 992px)").matches;
+  
+      $(".slick-slide img").each(function () {
+        const desktopSrc = $(this).attr("src");
+        const mobileSrc = $(this).data("mobile-src");
+  
+        if (isMobile) {
+          // Swap to mobile image
+          $(this).attr("src", mobileSrc);
+        } else {
+          // Revert to desktop image
+          $(this).attr("src", desktopSrc);
+        }
+      });
+    }
+  
+    // Initial check
+    updateImagesForMobile();
+  
+    // Re-check on window resize
+    $(window).on("resize", updateImagesForMobile);
   });
+  
   
